ea80759443319f9a5bcf3f66c18866343d2df96b
rmangra <richard.mangra@gmail.com>
2017-11-27 18:13:08 -0500

    Created a StopWatch class.

--- Exercises_Classes/Stopwatch/Stopwatch/Program.cs
+++ Exercises_Classes/Stopwatch/Stopwatch/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +9,7 @@ namespace Stopwatch
     {
         static void Main(string[] args)
         {
+            var stopwatch
         }
     }
 }
---
+++ Exercises_Classes/Stopwatch/Stopwatch/StopWatch.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Stopwatch
+{
+    /*
+     * https://msdn.microsoft.com/en-us/library/ff650316.aspx
+     */
+
+    public class StopWatch
+    {
+        private static volatile StopWatch instance;
+        private static object syncRoot = new Object();
+        private DateTime _startTime;
+        private DateTime _endTime;
+        private bool _running;
+
+        private StopWatch()
+        {
+            
+        }
+
+        public static StopWatch Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                            instance = new StopWatch();
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        public void Start()
+        {
+            if (_running)
+                throw new InvalidStopWatchException("Stopwatch is already running");
+            _startTime = DateTime.Now;
+            _running = true;
+
+        }
+
+        public void Stop()
+        {
+            if (!_running)
+                throw new InvalidStopWatchException("Stopwatch is not running");
+            _endTime = DateTime.Now;
+            _running = false;
+        }
+
+        public TimeSpan GetInterval()
+        {
+            return _endTime - _startTime;
+        }
+    }
+
+    /*
+     * https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/exceptions/creating-and-throwing-exceptions
+     */
+
+    [Serializable()]
+    public class InvalidStopWatchException : System.Exception
+    {
+        public InvalidStopWatchException() : base() { }
+        public InvalidStopWatchException(string message) : base(message) { }
+        public InvalidStopWatchException(string message, System.Exception inner) : base(message, inner) { }
+
+        // A constructor is needed for serialization when an
+        // exception propagates from a remoting server to the client. 
+        protected InvalidStopWatchException(System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context)
+        { }
+    }
+}
