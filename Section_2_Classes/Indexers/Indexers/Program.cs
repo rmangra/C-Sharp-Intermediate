30b029caf93926ec639c9783df9ed6bb607819e3
rmangra <richard.mangra@gmail.com>
2017-11-26 21:23:33 -0500

    Examples using Indexers.

---
+++ Section_2_Classes/Indexers/Indexers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cookie = new HttpCookie();
+            cookie["name"] = "Richard";
+            Console.WriteLine(cookie["name"]);
+        }
+    }
+}
