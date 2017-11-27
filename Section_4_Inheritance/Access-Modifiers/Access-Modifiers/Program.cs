8a7978f943d389aedac25d33f066f255d33ab49c
rmangra <richard.mangra@gmail.com>
2017-11-27 15:28:39 -0500

    Examples of Access Modifiers.

---
+++ Section_4_Inheritance/Access-Modifiers/Access-Modifiers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon;
+
+namespace Access_Modifiers
+{
+    public class GoldCustomer : Customer
+    {
+        public void OfferVoucher()
+        {
+            var rating = this.CalculateRating(excludeOrders: true);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer();
+        }
+    }
+}
---
+++ Section_4_Inheritance/Access-Modifiers/Amazon/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Amazon
+{
+    public class Customer
+    {
+        public class RateCalculator
+        {
+            public int Calculate(Customer customer)
+            {
+                return 0;
+            }
+        }
+        
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Promote()
+        {
+            var calculator = new RateCalculator();
+            var rating = calculator.Calculate(this);
+
+            Console.WriteLine("Promote logic changed.");
+
+        }
+
+        protected int CalculateRating(bool excludeOrders)
+        {
+            return 0;
+        }
+    }
+}
