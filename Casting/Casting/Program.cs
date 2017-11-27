faf7227d0eead192a1ff98cb90baca5e34c8d4dc
rmangra <richard.mangra@gmail.com>
2017-11-27 15:32:01 -0500

    Lecture example of upcasting.

---
+++ Casting/Casting/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Text text = new Text();
+            Shape shape = text;
+
+            text.Width = 200;
+            shape.Width = 100;
+
+            Console.WriteLine(text.Width);
+
+        }
+    }
+}
