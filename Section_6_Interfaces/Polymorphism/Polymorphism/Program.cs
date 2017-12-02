a304c411b5313460b832777a579b59343d7479d4
rmangra <richard.mangra@gmail.com>
2017-12-02 01:22:01 -0500

    Added necessary classes.

--- Section_6_Interfaces/Polymorphism/Polymorphism/Program.cs
+++ Section_6_Interfaces/Polymorphism/Polymorphism/Program.cs
@@ -11,6 +11,8 @@ namespace Polymorphism
         static void Main(string[] args)
         {
             var encoder = new VideoEncoder();
+            encoder.RegisterNotificationChannel(new MailNotificationChannel());
+            encoder.RegisterNotificationChannel(new SmsNotificationChannel());
             encoder.Encode(new Video());
         }
     }
