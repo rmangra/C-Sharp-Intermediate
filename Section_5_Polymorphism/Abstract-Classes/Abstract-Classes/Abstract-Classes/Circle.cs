df62ec28a2de320dbedc74b2567bfda132c3e817
rmangra <richard.mangra@gmail.com>
2017-12-02 01:41:34 -0500

    Added Circle and Rectangle class.

---
+++ Section_5_Polymorphism/Abstract-Classes/Abstract-Classes/Abstract-Classes/Circle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Abstract_Classes
+{
+    public class Circle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a circle");
+        }
+    }
+}
