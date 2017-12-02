050c4c27a18e33d7a1ad8ac28ad77b9abf35470b
rmangra <richard.mangra@gmail.com>
2017-12-01 21:58:26 -0500

    First commit

--- Exercises_Classes/Stopwatch/Stopwatch/Program.cs
+++ Exercises_Classes/Stopwatch/Stopwatch/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,16 @@ namespace Stopwatch
     {
         static void Main(string[] args)
         {
-            var stopwatch
+            var stopwatch = StopWatch.Instance;
+            for (var i = 0; i < 2; i++)
+            {
+                stopwatch.Start();
+                System.Threading.Thread.Sleep(1000);
+                stopwatch.Stop();
+                Console.WriteLine("Duration: " + stopwatch.GetInterval());
+                Console.WriteLine("Press Enter to run the stopwatch one more time.");
+                Console.ReadLine();
+            }
         }
     }
 }
