9b7649bca7c2a0ee0f9c7a0af1d93738e74689bc
rmangra <richard.mangra@gmail.com>
2017-12-02 01:45:47 -0500

    Examples using Abstract Classes.  These are useful when you want your design to be used everywhere in the codebase.

--- Section_5_Polymorphism/Abstract-Classes/Abstract-Classes/Abstract-Classes/Program.cs
+++ Section_5_Polymorphism/Abstract-Classes/Abstract-Classes/Abstract-Classes/Program.cs
@@ -1,9 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Abstract_Classes
+﻿namespace Abstract_Classes
 {
     class Program
     {
