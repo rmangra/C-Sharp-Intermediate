bcd4c38d8226b2a9b9d2cebfb1d1fae8d5137b54
rmangra <richard.mangra@gmail.com>
2017-12-07 08:53:01 -0500

    Exercise to design a database connection program.   It contains a base class called DbConnection, and two derived classes, SqlConnection and OracleConnection.

---
+++ Exercises_Polymorphism/DbConnection/DbConnection/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DbConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* For testing sqlconnection error */
+            // var sql = new SqlConnection(null, TimeSpan.Zero);   /* expect error */
+
+            var sql = new SqlConnection("localhost 3200", TimeSpan.Zero);
+            sql.Open();
+            sql.Close();
+            
+            // var oracle = new OracleConnection("localhost 3200", TimeSpan.Zero);
+            // oracle.Open();
+            // oracle.Close();
+        }
+    }
+}
---
+++ Exercises_Polymorphism/DbConnection/DbConnection/SqlConnection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DbConnection
+{
+    class SqlConnection : DbConnection
+    {
+        public override void Open(String value)
+        {
+            Console.WriteLine("Sql - Opening Connection... ");
+            Console.ReadLine();
+            this.ConnectionString = value;
+        }
+
+        public override void Open()
+        {
+            Console.WriteLine("Sql - Opening Connection... ");
+            Console.ReadLine();
+        }
+
+        public override void Close()
+        {
+            Console.WriteLine("Sql - Closing Connection... ");
+            Console.ReadLine();
+
+        }
+
+        public SqlConnection(String connectionstring, TimeSpan timeout) : base(connectionstring, timeout)
+        {
+
+        }
+    }
+}
