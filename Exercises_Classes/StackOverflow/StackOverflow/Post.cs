96b652c5492afcb7736be5d1dd487ecf4da3f836
rmangra <richard.mangra@gmail.com>
2017-12-06 23:06:24 -0500

    Exercise to simulate a stackoverflow post.  It has properties for title, description, and the date/time.  It also has the ability to up-vote and down-vote.  The aim is to demonstrate how classes encapsulate data and behavior around that data.

---
+++ Exercises_Classes/StackOverflow/StackOverflow/Post.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace StackOverflow
+{
+    class Post
+    {
+        private readonly String _title;
+        public String Title
+        {
+            get { return _title; }
+
+        }
+        private readonly String _description;
+        public String Description
+        {
+            get { return _description; }
+
+        }
+        private readonly DateTime _timestamp;
+        public DateTime TimeStamp
+        {
+            get { return _timestamp; }
+
+        }
+        private int _vote;
+        public int Vote
+        {
+            get { return _vote; }
+
+        }
+
+        private Post() { }
+        public Post(String title, String description, int vote)
+        {
+            this._title = title;
+            this._description = description;
+            this._timestamp = DateTime.Now;
+            this._vote = vote;
+        }
+        public int up_vote()
+        {
+
+            return ++_vote;
+
+        }
+        public int down_vote()
+        {
+            if (_vote > 0)
+                return --_vote;
+            return _vote;
+        }
+    }
+}
---
+++ Exercises_Classes/StackOverflow/StackOverflow/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var post = new Post("This is a test post", "The quick brown fox jumped over the lazy dog", 2);
+
+            Console.WriteLine("Title: " + post.Title);
+            Console.WriteLine("Description: " + post.Description);
+            Console.WriteLine("TimeStamp: " + post.TimeStamp);
+            Console.WriteLine("Up-Vote: " + post.up_vote());
+            Console.WriteLine("Down-Vote: " + post.down_vote());
+            Console.ReadLine();
+        }
+    }
+}
