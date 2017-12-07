aa9bfa1dbc294d337deceddc57455bda4da38bd7
rmangra <richard.mangra@gmail.com>
2017-12-06 23:40:11 -0500

    Exercise to create a stack data structure (LIFO).  It has a Stack class with the following methods: push, pop, peek, and clear.

---
+++ Exercises_Inheritance/StackDesign/StackDesign/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StackDesign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            int start = 0;
+            int end = 10;
+            for (int i = start; i <= end; i++)
+                stack.Push(i);
+
+            // this will clear the stack and cause an invalid stack exception
+            // stack.Clear();
+
+            // stack.Peek() returns top object; but not remove it from stack 
+            Console.WriteLine("Item at the top of the stack: " + stack.Peek());
+
+            for (int i = start; i <= end; i++)
+                Console.WriteLine(stack.Pop());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercises_Inheritance/StackDesign/StackDesign/Stack.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackDesign
+{
+    class Stack
+    {
+        private readonly List<object> _list = new List<object>();
+        public void Push(object obj)
+        {
+            if (obj == null)
+                throw new InvalidStackException("Cannot add a null object");
+            _list.Add(obj);
+        }
+        public object Pop()
+        {
+            if (_list.Count == 0)
+                throw new InvalidStackException("No elements in stack");
+            var index = _list.Count - 1;
+            var element = _list[index];
+            _list.RemoveAt(index);
+            return element;
+
+        }
+        public object Peek()
+        {
+            if (_list.Count == 0)
+                throw new InvalidStackException("No elements in stack");
+            var index = _list.Count - 1;
+            var element = _list[index];
+            return element;
+        }
+        public void Clear()
+        {
+            _list.Clear();
+        }
+    }
+    /*
+     * https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/exceptions/creating-and-throwing-exceptions
+     */
+
+    [Serializable()]
+    public class InvalidStackException : System.Exception
+    {
+        public InvalidStackException() : base() { }
+        public InvalidStackException(string message) : base(message) { }
+        public InvalidStackException(string message, System.Exception inner) : base(message, inner) { }
+
+        // A constructor is needed for serialization when an
+        // exception propagates from a remoting server to the client. 
+        protected InvalidStackException(System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context)
+        { }
+    }
+
+
+}
